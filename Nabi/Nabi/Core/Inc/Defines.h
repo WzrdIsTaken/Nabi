9a9492867346ac76293e3a6f8a526023617e0c53
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2023-06-29 21:38:39 +0100

    doing some debugging, pushing these for ease

--- Nabi/Nabi/Core/Inc/Defines.h
+++ Nabi/Nabi/Core/Inc/Defines.h
@@ -4,11 +4,12 @@
 
 // Core defines
 #define USE_EVENT_SYSTEM_UPDATE
-#define USE_CORE_FUNCTIONALITY_MULTITHREADING
+//#define USE_CORE_FUNCTIONALITY_MULTITHREADING
 //#define USE_NABI_NOEXCEPT
 
 // Debug defines
 #ifdef _DEBUG
+	#define INCLUDE_DEMO     // Demo of all Nabi's features working together
 	#define RUN_TESTS		 // gMock Tests
 	//#define RUN_SLOW_TESTS   // Any tests which take over ~10ms to run. Make sure to run before commiting
 	#define USE_DEBUG_UTILS  // Asserts + Logging + Sneaky hacks
@@ -17,11 +18,11 @@
 #endif // ifdef _DEBUG
 
 // Value defines
-#define NABI_SUCCESS 0
-#define NABI_FAIL   -1
+#define NABI_SUCCESS EXIT_SUCCESS 
+#define NABI_FAIL    EXIT_FAILURE
 
 // Development defines
-#define ENGINE_DEVELOPMENT
+//#define ENGINE_DEVELOPMENT
 
 // Defines based off other defines!
 #ifndef USE_NABI_NOEXCEPT
--- Nabi/Nabi/Tests/Src/Demo/ECS/Systems/AsteroidSystem.cpp
+++ Nabi/Nabi/Tests/Src/Demo/ECS/Systems/AsteroidSystem.cpp
@@ -23,17 +23,10 @@ namespace ecs
 	void AsteroidSystem::Update(nabi::GameTime const& gameTime)
 	{
 		// TODAY
-		// - Need a ToString function for types, eg for XMFLOAT3, which turns them into a relfection friendly string repretentation
-		//	- should this be a reflected func?
-		//  - or just a templated one with that new approach idea?
-		// - I think the reason that OnDemoBegin isn't being called is because the sytems only get constructed at the end of the reflection?
-		//	- gotta think of a nice solution for this
-		// - And have to sort out how we are going to store game singletoncomponents
+		// Need to write an asset bank for the Demo. Need the functions which will turn eg ModelComponent to buffer components
 
 		// so on the bad side... a lot of todos..
 		// but on the good side - im glad we are doing this now. come on ben - you got this!!
-
-		// Need to write an asset bank for the Demo. Need the functions which will turn eg ModelComponent to buffer components
 	}
 } // namespace ecs
 
