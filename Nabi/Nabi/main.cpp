fe55204bfe8938151182163b6e9e9ffaa85567e5
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2023-07-07 12:16:29 +0100

    now we're ready!

--- Nabi/Nabi/main.cpp
+++ Nabi/Nabi/main.cpp
@@ -11,13 +11,13 @@
              ||   ||   ||                ||   ||( '-||-' )
              || ,;.)   (.;,            ,;.)   (.;,(~\/~)/
 
-	These are the header guards. They guard every header in this project, keeping the lands of Nabi safe.
+	These are the header guards, protecting this project and keeping the lands of Nabi safe.
 */
 
 #include "Main.h"
 
 int CALLBACK wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {
-    // Your game here!
+    // Your game here! glhf
     return 0;
 }
